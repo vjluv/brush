5e4c4260399d2fb12b18269bba5a49536718ffac
vjluv <153458870+vjluv@users.noreply.github.com>
2023-12-10 15:09:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(250);
 
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('https://vjluv.github.io/brush/');
   socket.on('mouse', newDrawing);
 }
 
