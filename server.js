9d262f65a6877fe4b6baf56c9ca37a045c00456a
vjluv <153458870+vjluv@users.noreply.github.com>
2023-12-10 19:41:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ console.log("My socket server is running");
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://vjluv.github.io',
+    origin: 'https://vjluv.github.io/brush',
     methods: ["GET", "POST"],
     credentials: true,
   },
