cbf6183e452f26ae3905d935f5bac06d58f5a710
vjluv <153458870+vjluv@users.noreply.github.com>
2023-12-10 19:41:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var app = express();
-var server = app.listen(3000);
+const express = require('express');
+const app = express();
+const server = app.listen(3000);
 
 app.use(express.static('public'));
 
 console.log("My socket server is running");
 
-var socket = require('socket.io');
-var io = socket(server, {
+const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://vjluv.github.io/brush';
-    methods: ["GET", "POST"];
+    origin: 'https://vjluv.github.io',
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
-const socket = io.connect('https://vjluv.github.io/brush'); // Substitua com a URL correta
 
 io.sockets.on('connection', newConnection);
 
@@ -28,3 +26,4 @@ function newConnection(socket) {
     console.log(data);
   }
 }
+
